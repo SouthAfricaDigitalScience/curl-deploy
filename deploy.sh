4dfc78ffbc334189d4240c82be36e79fb0f12363
Bruce Becker <brucellino@gmail.com>
2017-05-03 19:13:56 +0200

    Minor Release Update
      1. Add license text to all files
      1. Update module variable for new containers

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,18 @@
 #!/bin/bash -e
+# Copyright 2016 C.S.I.R. Meraka Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # this should be run after check-build finishes.
 . /etc/profile.d/modules.sh
 echo ${SOFT_DIR}
@@ -11,8 +25,8 @@ echo "All tests have passed, will now build into ${SOFT_DIR}"
 ../configure \
 --prefix=${SOFT_DIR}
 make install -j2
-echo "Creating the modules file directory ${LIBRARIES_MODULES}"
-mkdir -p ${LIBRARIES_MODULES}/${NAME}
+echo "Creating the modules file directory ${LIBRARIES}"
+mkdir -p ${LIBRARIES}/${NAME}
 (
 cat <<MODULE_FILE
 #%Module1.0
@@ -31,7 +45,7 @@ prepend-path LD_LIBRARY_PATH   $::env(CURL_DIR)/lib
 prepend-path CFLAGS                       "-I${CURL_DIR}/include"
 prepend-path LDFLAGS                     "-L${CURL_DIR}/lib -lcurl"
 MODULE_FILE
-) > ${LIBRARIES_MODULES}/${NAME}/${VERSION}
+) > ${LIBRARIES}/${NAME}/${VERSION}
 module avail ${NAME}
 module add ${NAME}/${VERSION}
 which curl
